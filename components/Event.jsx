9c9741d31eb3e15d2204108888f115f612ea41e2
danielfdz123 <daniel.fernandez16@myhunter.cuny.edu>
2025-02-27 20:22:48 -0500

    Fixed some props to make organization easier; buttons are now clickable and take you to buy tickets for the corresponding matches

--- components/Event.jsx
+++ components/Event.jsx
@@ -6,8 +6,10 @@ const Event = (props) => {
             <img className = "flag" src={"/images/" + props.team1.toLowerCase() + ".webp"}/>
             <img className = "flag" src={"/images/" + props.team2.toLowerCase() + ".webp"}/>
             <h2> {props.team1} Vs. {props.team2} </h2>
-            <p className = "link"> {props.date} <br></br>{props.time} </p>
-            <button className = "moreInfo"> More Info </button>
+            <p class = "info"> <b class = "location"> 🏟️ {props.venue} <br></br>📍 {props.location} </b> <br></br> ⏰ {props.date} at {props.time} </p>
+            <a href = {props.tickets}>
+                <button className = "link"> Get Tickets!</button>
+            </a>
         </div>
     );
 };
