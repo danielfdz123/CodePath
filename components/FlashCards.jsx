014437955e54a45f7a3ee9924fc7ba4acbd37ca0
danielfdz123 <daniel.fernandez16@myhunter.cuny.edu>
2025-03-08 01:28:36 -0500

    added more prompts/pictures, and adjusted some styling

--- components/FlashCards.jsx
+++ components/FlashCards.jsx
@@ -6,13 +6,19 @@ const FlashCards = ({ prompt }) => {
     const flipCard = () => {
         setIsFlipped(!isFlipped); 
     };
-    
+
+    const difficultyClasses = {
+        easy: "green",
+        medium: "yellow",
+        hard: "red",
+    };
+
     return (
         <div className = "flip-card" onClick={flipCard}>
-            <div className={`flip-card-inner ${isFlipped ? 'flipped' : ''}`}>
-
+            <div className = {`flip-card-inner ${isFlipped ? 'flipped' : ''} ${difficultyClasses[prompt.difficulty]}`}>
+        
                 {/* Front Side of Card */}
-                <div className = "flip-card-front">
+                <div className = {`flip-card-front ${difficultyClasses[prompt.difficulty]}`}>
                     <h3>{prompt.q}</h3>
                     <ul>
                         <li>{prompt.h1}</li>
@@ -22,12 +28,13 @@ const FlashCards = ({ prompt }) => {
                 </div>
 
                 {/* Back Side of Card */}
-                <div className = "flip-card-back">
+                <div className = {`flip-card-back ${difficultyClasses[prompt.difficulty]}`}>
                     <h3>{prompt.a}</h3>
+                    <img className = "player" src={`/public/${prompt.img}.webp`} alt={prompt.a} />
                 </div>
             </div>
         </div>
-  );
+    );
 };
 
 export default FlashCards;
